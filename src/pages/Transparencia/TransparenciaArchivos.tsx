c9cfd2b894529b2e2f107b0922340f7b69bdef0d
Enrique Alejo <enriquealejo28@gmail.com>
2025-04-14 12:05:05 -0600

    implementacion del acta de entrega

--- src/pages/Transparencia/TransparenciaArchivos.tsx
+++ src/pages/Transparencia/TransparenciaArchivos.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row, Table } from "antd"
+import { Button, Col, Row, Space, Table } from "antd"
 import { FilePdfOutlined } from '@ant-design/icons';
 export const TransparenciaArchivos = () => {
   const dataSource = [
@@ -7,28 +7,32 @@ export const TransparenciaArchivos = () => {
       obra: "REHABILITACION DE RED DE AGUA ENTUBADA EN CALLE EL POSTECITO, EN LA RANCHERIA ORIENTE SEGUNDA SECCIÓN (PALMA HUACA), DEL MUNICIPIO DE PARAISO; TABASCO. (TERRESTRE 2023)",
       localidad: "270140049. RA. ORIENTE 2DA. SECCION . (PALMA HUACA).",
       contrato: "CO-PA-HIDRCTERR-022-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-022-2024%20OBRA%20OPM57_redacted.pdf"
+      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-022-2024%20OBRA%20OPM57_redacted.pdf",
+      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20POSTECITO.pdf"
     },
     {
       proyecto: "OPM56",
       obra: "REHABILITACION DE CALLE A BASE DE GRAVA DE REVESTIMIENTO DE 1 1/2 A FINOS, (UBICACIÓN ENTRADA LOS GÓMEZ) EN LA RANCHERIA LAS FLORES PRIMERA SECCIÓN, DEL MUNICIPIO DE PARAISO; TABASCO. (MARITIMA 2023)",
       localidad: "270140009. RA. LAS FLORES 1RA. SECCION",
       contrato: "CO-PA-HIDRCMAR-021-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-021-2024%20OBRA%20OPM56_redacted.pdf"
+      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-021-2024%20OBRA%20OPM56_redacted.pdf",
+      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20RECEPCION%20FISICA%20%20LOS%20GOMEZ.pdf",
     },
     {
       proyecto: "OPM59",
       obra: "REHABILITACION DE CALLE A BASE DE CONCRETO ASFALTICO EN CALIENTE (UBICACIÓN ENTRADA AL BACHILLER), EN LA RANCHERÍA POTRERITOS DEL MUNICIPIO DE PARAISO; TABASCO. (MARITIMA 2024)",
       localidad: "270140026. RA. POTRERITOS",
       contrato: "CO-PA-HIDRCMAR-024-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-024-2024%20OBRA%20OPM59_redacted.pdf "
+      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-024-2024%20OBRA%20OPM59_redacted.pdf",
+      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20%20POTRERITO.pdf",
     },
     {
       proyecto: "OPM58",
       obra: "REHABILITACION DE CAMINO A BASE DE CONCRETO ASFALTICO EN CALIENTE EN EL EJIDO OCCIDENTE SAN FRANCISCO, DEL MUNICIPIO DE PARAISO; TABASCO. (TERRESTRE 2024)",
       localidad: "270140050. EJ. OCCIDENTE (SAN FRANCISCO).",
       contrato: "CO-PA-HIDRCTERR-023-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-023-2024%20OBRA%20OPM58_redacted.pdf "
+      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-023-2024%20OBRA%20OPM58_redacted.pdf ",
+      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20SAN%20FRANCISCO.pdf",
     },
     {
       proyecto: "KAM52",
@@ -41,7 +45,7 @@ export const TransparenciaArchivos = () => {
       obra: "MEJORA DE RED DE DISTRIBUCION DE ENERGIA ELECTRICA EN MEDIA Y BAJA TENSION EN EL EJIDO ANDRES GARCIA (LA ISLA) DEL MUNICIPIO DE PARAISO, TABASCO",
       localidad: "270140045. EJ. ANDRES GARCIA (LA ISLA)",
       contrato: "CO-PA-R23TER-008-2025"
-    }
+    },
   ]
 
   const columns = [
@@ -49,38 +53,65 @@ export const TransparenciaArchivos = () => {
       title: 'Proyecto No.',
       dataIndex: 'proyecto',
       key: 'proyecto',
+      width: "5%",
     },
     {
       title: 'Obra',
       dataIndex: 'obra',
       key: 'obra',
+      width: "20%",
     },
     {
       title: 'Localidad',
       dataIndex: 'localidad',
       key: 'localidad',
+      width: "10%",
     },
     {
       title: 'Contrato',
       dataIndex: 'contrato',
       key: 'contrato',
+      width: "6%",
     },
     {
       title: 'Archivo',
-      dataIndex: 'archivo',
       key: 'archivo',
+      dataIndex: 'archivo',
+      width: "2%",
       render: (e: any) => (
-        (e) ? < Button
-          icon={< FilePdfOutlined />}
-          href={e}
-          target="_blank"
-        />
+
+        (e)
+          ? < Button
+            icon={< FilePdfOutlined />}
+            href={e}
+            target="_blank"
+          >
+            archivo
+          </ Button>
+          : <></>
+      ),
+    },
+    {
+      title: 'Actas de entrega',
+      key: 'archivoFisico',
+      dataIndex: 'archivoFisico',
+      width: "2%",
+      render: (e: any) => (
+
+        (e)
+          ? < Button
+            icon={< FilePdfOutlined />}
+            href={e}
+            target="_blank"
+          >
+            Acta de entrega
+          </ Button>
           : <></>
       ),
     },
   ];
   return (
-    <div style={{ height: "80vh" }} >
+    <div  >
       <h2 className="tituloInicio">
         Archivos Publicados
       </h2>
