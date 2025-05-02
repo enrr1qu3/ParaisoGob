6de58b52409fdc0c63f73cab6ef3cf304d430fa2
Enrique Alejo <enriquealejo28@gmail.com>
2025-05-02 15:23:57 -0600

    cambio de los url aws

--- src/pages/Transparencia/TransparenciaArchivos.tsx
+++ src/pages/Transparencia/TransparenciaArchivos.tsx
@@ -7,38 +7,38 @@ export const TransparenciaArchivos = () => {
       obra: "REHABILITACION DE RED DE AGUA ENTUBADA EN CALLE EL POSTECITO, EN LA RANCHERIA ORIENTE SEGUNDA SECCIÓN (PALMA HUACA), DEL MUNICIPIO DE PARAISO; TABASCO. (TERRESTRE 2023)",
       localidad: "270140049. RA. ORIENTE 2DA. SECCION . (PALMA HUACA).",
       contrato: "CO-PA-HIDRCTERR-022-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-022-2024%20OBRA%20OPM57_redacted.pdf",
-      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20POSTECITO.pdf"
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/Contrato_No._CO-PA-HIDRCTERR-022-2024_OBRA_OPM57_redacted.pdf",
+      archivoFisico: "https://gobparaiso.blob.core.windows.net/transparencia/ACTA_ENTREGA_FISICA_POSTECITO.pdf"
     },
     {
       proyecto: "OPM56",
       obra: "REHABILITACION DE CALLE A BASE DE GRAVA DE REVESTIMIENTO DE 1 1/2 A FINOS, (UBICACIÓN ENTRADA LOS GÓMEZ) EN LA RANCHERIA LAS FLORES PRIMERA SECCIÓN, DEL MUNICIPIO DE PARAISO; TABASCO. (MARITIMA 2023)",
       localidad: "270140009. RA. LAS FLORES 1RA. SECCION",
       contrato: "CO-PA-HIDRCMAR-021-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-021-2024%20OBRA%20OPM56_redacted.pdf",
-      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20RECEPCION%20FISICA%20%20LOS%20GOMEZ.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/Contrato_No._CO-PA-HIDRCMAR-021-2024_OBRA_OPM56_redacted.pdf",
+      archivoFisico: "https://gobparaiso.blob.core.windows.net/transparencia/ACTA_RECEPCION_FISICA_LOS_GOMEZ.pdf",
     },
     {
       proyecto: "OPM59",
       obra: "REHABILITACION DE CALLE A BASE DE CONCRETO ASFALTICO EN CALIENTE (UBICACIÓN ENTRADA AL BACHILLER), EN LA RANCHERÍA POTRERITOS DEL MUNICIPIO DE PARAISO; TABASCO. (MARITIMA 2024)",
       localidad: "270140026. RA. POTRERITOS",
       contrato: "CO-PA-HIDRCMAR-024-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCMAR-024-2024%20OBRA%20OPM59_redacted.pdf",
-      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20%20POTRERITO.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/Contrato_No._CO-PA-HIDRCMAR-024-2024_OBRA_OPM59_redacted.pdf",
+      archivoFisico: "https://gobparaiso.blob.core.windows.net/transparencia/ACTA_ENTREGA_FISICA_POTRERITO.pdf",
     },
     {
       proyecto: "OPM58",
       obra: "REHABILITACION DE CAMINO A BASE DE CONCRETO ASFALTICO EN CALIENTE EN EL EJIDO OCCIDENTE SAN FRANCISCO, DEL MUNICIPIO DE PARAISO; TABASCO. (TERRESTRE 2024)",
       localidad: "270140050. EJ. OCCIDENTE (SAN FRANCISCO).",
       contrato: "CO-PA-HIDRCTERR-023-2024",
-      archivo: "https://paraisomovimiento.com/transparencia/Contrato%20No.%20CO-PA-HIDRCTERR-023-2024%20OBRA%20OPM58_redacted.pdf ",
-      archivoFisico: "https://paraisomovimiento.com/transparencia/ACTA%20ENTREGA%20FISICA%20SAN%20FRANCISCO.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/Contrato_No._CO-PA-HIDRCTERR-023-2024_OBRA_OPM58_redacted.pdf",
+      archivoFisico: "https://gobparaiso.blob.core.windows.net/transparencia/ACTA_ENTREGA_FISICA_SAN_FRANCISCO.pdf",
     },
     {
       proyecto: "KAM52",
       obra: "MEJORA DE RED DE DISTRIBUCION DE ENERGIA ELECTRICA EN MEDIA Y BAJA TENSION EN LA COLONIA EL BELLOTE (MIGUEL DE LA MADRID), UBICADA ENTRADA ANTES LA PANGA) DEL MUNICIPIO DE PARAISO, TABASCO",
       localidad: "270140004. COL EL BELLOTE. (MIGUEL DE LA MADRID).",
-      contrato: "CO-PA- R23MAR-007-2025"
+      contrato: "CO-PA-R23MAR-007-2025"
     },
     {
       proyecto: "KAM53",
@@ -50,37 +50,37 @@ export const TransparenciaArchivos = () => {
   const dataArt46 = [
     {
       nombre: "Publicacion Programacion",
-      archivo: "https://paraisomovimiento.com/transparencia/PUBLICACION%20PROGRAMACION.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/PUBLICACION_PROGRAMACION.pdf",
     },
     {
       nombre: "Clasificacion de economica(por tipo de gasto)",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20de%20economica%20(por%20tipo%20de%20gasto).pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_de_economica_(por_tipo_de_gasto).pdf",
     },
     {
       nombre: "Clasificacion funcional",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20funcional.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_funcional.pdf",
     },
     {
       nombre: "Clasificacion administrativa",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20administrativa.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_administrativa.pdf",
     },
     {
       nombre: "Clasificacion por objeto del gasto",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20por%20objeto%20del%20gasto.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_por_objeto_del_gasto.pdf",
     },
     {
       nombre: "Clasificacion por categoria programatica",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20por%20categoria%20programatica.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_por_categoria_programatica.pdf",
     },
     {
       nombre: "Clasificacion por programas y proyectos",
-      archivo: "https://paraisomovimiento.com/transparencia/clasificacion%20de%20programas%20y%20proyectos.pdf",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/clasificacion_de_programas_y_proyectos.pdf",
     },
   ]
   const dataArt72y75 = [
     {
       nombre: "FAIS Y FORTAMUN 2025",
-      archivo: "https://paraisomovimiento.com/transparencia/FAIS%20Y%20FORTAMUN%202025.docx",
+      archivo: "https://gobparaiso.blob.core.windows.net/transparencia/FAIS_Y_FORTAMUN_2025.pdf",
     },
 
   ]
@@ -243,7 +243,7 @@ export const TransparenciaArchivos = () => {
           <Table
             title={() => <h3 style={{ margin: 0 }}>UNIDAD EVALUACIÓN DE DESEMPEÑO MUNICIPAL. (UEDM)</h3>}
             dataSource={dataUEDM}
-            size="small" 
+            size="small"
             columns={columnsDOS}
             rowKey={(record) => record.nombre}
             scroll={{ x: 'max-content' }}
