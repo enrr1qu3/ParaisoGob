7729a7789aaa05f658fa70761442fe6886a81c5d
Enrique <enriquealejo28@gmail.com>
2025-07-29 22:25:16 -0600

    Implementacion correcta del video en el carrusel

--- src/pages/Inicio/components/Anuncios.tsx
+++ src/pages/Inicio/components/Anuncios.tsx
@@ -27,86 +27,21 @@ const anuncios = [
 ];
 
 export const Anuncios = () => {
-    // return (
-    //     <Card style={{ margin: "3rem 0rem 0rem 0rem ", }}>
-    //         <h2 className='tituloSeccion'>Anuncios</h2>
-    //         <Row gutter={[8, 8]} style={{ marginTop: 10, textAlign: "center" }}>
-    //             <Col
-    //                 xs={{ flex: "100%" }}
-    //                 md={{ flex: "50%" }}
-    //                 xl={{ flex: "33.3%" }}
-    //             >
-
-    //                 <Card
-
-    //                     cover={
-    //                         <Image  alt='Anuncio' src='https://scontent.fvsa2-1.fna.fbcdn.net/v/t39.30808-6/524129252_122183398922505987_1554585239385295218_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGqZYKDbjXxb8Gdt3wb3a4v3T7Qh6ZWNRPdPtCHplY1E0yH1eDFrHcdMvWs0hmvfW0bb4BT3_Chqbi7z7Yp-QRw&_nc_ohc=gfkqXUmjYE4Q7kNvwGRjMuH&_nc_oc=Admbz1UUhc8B1UzOIGLTkRMm2G2ZxV2wlKSP8_I-dUoxzFQ1TjF8zBp2B_diqfvAAil9_KOlmWgt8HIUuyZKz_HG&_nc_zt=23&_nc_ht=scontent.fvsa2-1.fna&_nc_gid=2dNuh2OMjt80ldLihcFEDQ&oh=00_AfS1-AJF1W2y5Zx9RrLEhTCtcpr7HFaPIPG3dOlDVFbAXA&oe=688DF79F' />
-    //                     }
-    //                 >
-    //                     <Meta description={
-    //                         <div className='metaCard' >
-    //                             <Button size='large' type='dashed' href="https://www.facebook.com/share/p/1FYJkfb1hg/?mibextid=wwXIfr" target="_blank">Más Informacion</Button>
-    //                         </div>
-    //                     } />
-    //                 </Card>
-    //             </Col>
-    //             <Col
-    //                 xs={{ flex: "100%" }}
-    //                 md={{ flex: "50%" }}
-    //                 xl={{ flex: "33.3%" }}
-    //             >
-
-    //                 <Card
-
-    //                     cover={
-    //                         <Image  alt='Anuncio' src='https://scontent.fvsa2-1.fna.fbcdn.net/v/t39.30808-6/524922430_122183390840505987_299499171823405578_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHXmejr2pMQkm0rRxx64JKuQM2ktkRdySBAzaS2RF3JIK4o_b7sAOLrCJIz1oqI4lViY_Zv1RsnIY_aqCJ69YuZ&_nc_ohc=DCziLz-AXNQQ7kNvwGNTFYx&_nc_oc=AdlctgHUX90OXHL6BuEt3B9wb_SpEyrnZw2GHHdQ2nYVcs2_hcjDSzc6BDJRMVElXC-hSk0nIdIu7RE7ydBFOhib&_nc_zt=23&_nc_ht=scontent.fvsa2-1.fna&_nc_gid=6ZTDIxcZsbQFCfFjX7VbDA&oh=00_AfT2IIbcDxjOEZApTgUdgLMOz0ANgXW2gYKzaseai53ceg&oe=688E0C50' />
-    //                     }
-    //                 >
-    //                     <Meta description={
-    //                         <div className='metaCard' >
-    //                             <Button size='large' type='dashed' href="https://www.facebook.com/share/p/1AioG9Regh/?mibextid=wwXIfr" target="_blank">Más Informacion</Button>
-    //                         </div>
-    //                     } />
-    //                 </Card>
-    //             </Col>
-    //             <Col
-    //                 xs={{ flex: "100%" }}
-    //                 md={{ flex: "50%" }}
-    //                 xl={{ flex: "33.3%" }}
-    //             >
-
-    //                 <Card
-
-    //                     cover={
-    //                         <Image  alt='Anuncio' src='https://scontent.fvsa2-1.fna.fbcdn.net/v/t39.30808-6/524754644_122183384216505987_3481552598953378711_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeE6L5Hx45L4Js7EFnPmUqwGzYe6bBw_e9rNh7psHD972rsDdSVW7-3YnGBtvW-7ByrBBzDzDRlWt1EW8bK2k5lV&_nc_ohc=QBz2uf38xM0Q7kNvwHbXagb&_nc_oc=AdlUpoZoGkDvgXgFvc9L6AmgZvYzNLxARVJte3l1DLrBwH01lZErnIJwtQVU9znV04K2u63xbX2lw1VUPyJANCdx&_nc_zt=23&_nc_ht=scontent.fvsa2-1.fna&_nc_gid=Eeohxf75o0IuNztdgcTv4Q&oh=00_AfRVxmTcpOuHe6AI9bejs6dumAat-rOlJGMHGQEOpSBbZA&oe=688E14B7' />
-    //                     }
-    //                 >
-    //                     <Meta description={
-    //                         <div className='metaCard' >
-    //                             <Button size='large' type='dashed' href="https://www.facebook.com/share/p/1EYVgcSTRA/?mibextid=wwXIfr" target="_blank">Más Informacion</Button>
-    //                         </div>
-    //                     } />
-    //                 </Card>
-    //             </Col>
-    //         </Row>
-
-
-    //     </Card>
-    // )
     return (
 
         <section style={{ textAlign: 'center', padding: '40px 20px' }}>
-            <Title level={2} style={{ color: '#F26C0D' }}>Anuncios</Title>
-            <Paragraph style={{ color: '#555' }}>Avisos importantes y servicios municipales</Paragraph>
+            <div className='contenedorTitulo'>
+                <h2 className='tituloSeccion'>Anuncios</h2>
+            </div>
 
             <Row gutter={[24, 24]} justify="center">
                 {anuncios.map(item => (
                     <Col key={item.id} xs={24} sm={12} md={8}>
                         <Card
                             hoverable
-                            cover={<Image  style={{ aspectRatio:"7  /6" }} alt={item.title} src={item.image} />}
+                            cover={<Image style={{ aspectRatio: "7  /6" }} alt={item.title} src={item.image} />}
                             bodyStyle={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-                            style={{ borderRadius: 12 }}
+                            style={{ borderRadius: 12, cursor:"default" }}
                         >
                             <Title level={4} style={{ color: '#1A4B8C', textAlign: 'center' }}>
                                 {item.title}
--- src/pages/Inicio/components/SwiperInicio.tsx
+++ src/pages/Inicio/components/SwiperInicio.tsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
+
+import { ConfigProvider, Modal } from "antd";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
-import '../style/SwiperInicio.css'
-import { Modal } from "antd";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import '../style/SwiperInicio.css';
+
 export const SwiperInicio = () => {
+
+    const slides = [
+        {
+            type: "video",
+            src: "https://gobparaiso.blob.core.windows.net/home/Playa%20del%20Sol.mp4", // demo video
+            title: "PUERTO DEL SOL",
+            subtitle: "PARAISO",
+            description: "Un recorrido increíble por nuestros paisajes.",
+            buttonText: "Más informacio",
+            buttonLink: "#",
+        },
+        {
+            type: "image",
+            src: "/Cangrejo_Ceiba.png",
+            alt: "Perro",
+            title: "PUERTO CEIBA",
+            subtitle: "MALECÓN DEL CANGREJO AZUL",
+            description: "Visita nuestros atractivos turísticos y disfruta de la gastronomía local.",
+            buttonText: "Explorar",
+            buttonLink: "#"
+        },
+        {
+            type: "image",
+            src: "/Puente_Bellote.png",
+            alt: "Montaña",
+            title: "PUENTE EL BELLOTE",
+            subtitle: "PUENTE EL BELLOTE",
+            description: "Visita nuestros atractivos turísticos.",
+            buttonText: "Conoce más",
+            buttonLink: "#"
+        },
+    ];
+
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const showModal = () => {
@@ -17,145 +54,92 @@ export const SwiperInicio = () => {
     const handleCancel = () => {
         setIsModalOpen(false);
     };
-
     return (
-        <>
-            <Modal
-                title="Playa del Sol"
-                closable={{ 'aria-label': 'Custom Close Button' }}
-                open={isModalOpen}
-                onOk={handleOk}
-                onCancel={handleCancel}
-                footer={false}
+        <div style={{ width: "100%", height: "35rem" }}>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Modal: {
+                            contentBg: "#d5d5d5",
+                            headerBg: "#d5d5d5",
+                            titleColor: "#1c9dd9"
+                        },
+                    },
+                }}
             >
-                <p>PLAYA DEL SOL, EL DESTINO FAMILIAR POR EXCELENCIA EN PARAÍSO
-                    TURISMO, SEGURIDAD Y SERVICIOS EN UN SOLO LUGAR
-                </p>
-                <p>Con espacios cómodos, seguros y completamente equipados, Playa del Sol se consolida como uno de los principales atractivos turísticos de Tabasco, ofreciendo a visitantes y familias una experiencia inolvidable frente al mar.
-                </p>
-                <p>
-                    Ubicada en el municipio de Paraíso, esta playa cuenta con zonas de palapas tipo sombrilla a solo $200 pesos, secciones amplias con mesa y sillas por $300, y un domo principal para eventos con costo variable según las necesidades del cliente. Además, se brinda atención para bodas, celebraciones especiales y reuniones familiares.
-
-                </p>
-                <p>Para quienes visitan en vehículo, el área de estacionamiento tiene tarifas accesibles que van desde $30 pesos para motocicletas, hasta $300 pesos para autobuses. También se dispone de baños con regaderas y vestidores por solo $5 pesos, garantizando comodidad y servicios higiénicos a todos los visitantes.
-                </p>
-                <p>Además del encanto natural de sus paisajes, Playa del Sol ofrece seguridad permanente, atención de salvavidas, limpieza constante y acceso a los más deliciosos sabores de playa, todo en un entorno 100% familiar.
-                </p>
-                <p>
-                    La administración municipal invita a las y los tabasqueños a disfrutar de este espacio diseñado para el descanso, la convivencia y el desarrollo turístico del estado.
-                </p>
-            </Modal>
+                <Modal
+                    title="PLAYA DEL SOL"
+                    closable={{ 'aria-label': 'Custom Close Button' }}
+                    open={isModalOpen}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
+                    footer={false}
+                >
+                    <p>PLAYA DEL SOL, EL DESTINO FAMILIAR POR EXCELENCIA EN PARAÍSO
+                        TURISMO, SEGURIDAD Y SERVICIOS EN UN SOLO LUGAR
+                    </p>
+                    <p>Con espacios cómodos, seguros y completamente equipados, Playa del Sol se consolida como uno de los principales atractivos turísticos de Tabasco, ofreciendo a visitantes y familias una experiencia inolvidable frente al mar.
+                    </p>
+                    <p>
+                        Ubicada en el municipio de Paraíso, esta playa cuenta con zonas de palapas tipo sombrilla a solo $200 pesos, secciones amplias con mesa y sillas por $300, y un domo principal para eventos con costo variable según las necesidades del cliente. Además, se brinda atención para bodas, celebraciones especiales y reuniones familiares.
+                    </p>
+                    <p>Para quienes visitan en vehículo, el área de estacionamiento tiene tarifas accesibles que van desde $30 pesos para motocicletas, hasta $300 pesos para autobuses. También se dispone de baños con regaderas y vestidores por solo $5 pesos, garantizando comodidad y servicios higiénicos a todos los visitantes.
+                    </p>
+                    <p>Además del encanto natural de sus paisajes, Playa del Sol ofrece seguridad permanente, atención de salvavidas, limpieza constante y acceso a los más deliciosos sabores de playa, todo en un entorno 100% familiar.
+                    </p>
+                    <p>
+                        La administración municipal invita a las y los tabasqueños a disfrutar de este espacio diseñado para el descanso, la convivencia y el desarrollo turístico del estado.
+                    </p>
+                </Modal>
+            </ConfigProvider>
             <Swiper
-                cssMode={true}
-                navigation={true}
-                pagination={true}
-                keyboard={true}
-                loop={true}
-                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                className="contenedorSwiper"
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={0}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 12000, disableOnInteraction: false }}
+                loop
+                style={{ width: "100%", height: "100%" }}
             >
-                <SwiperSlide
-                    className="contenedorVideo"
-                >
-                    <video src="https://gobparaiso.blob.core.windows.net/home/Playa%20del%20Sol.mp4" loop autoPlay muted></video>
-                    <div className="cajaSombra" style={{backgroundColor:"#FFF"}}>
-                        <div className="container containerTextos">
-                            <div className="contenedorCaja">
-                                <h2 className="swiperTextoTitulo" >Playa del Sol</h2>
-                                <h3 className="swiperTextoSubTitulo">PARAISO</h3>
-                                <p className="swiperParrafo">
-                                    Visita nuestros atractivos turísticos.
-                                </p>
-                                <button className="swiperBoton"
-                                    onClick={showModal}
-                                >Conoce más</button>
+                {slides.map((slide, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="slide-content">
+                            {/* Imagen o video según el tipo */}
+                            {slide.type === "image" ? (
+                                <img src={slide.src} alt={slide.title} className="slide-img" />
+                            ) : (
+                                <video
+                                    className="slide-video"
+                                    src={slide.src}
+                                    autoPlay
+                                    loop
+                                    muted
+                                    playsInline
+                                />
+                            )}
+
+                            {/* Overlay en toda la imagen/video */}
+                            <div className="overlay"></div>
+
+                            {/* Texto al centro */}
+                            <div className="slide-text">
+                                <h2>{slide.title}</h2>
+                                <h4>{slide.subtitle}</h4>
+                                <p>{slide.description}</p>
+
+                                {slide.type === "image" ? (
+                                    <a href={slide.buttonLink} className="btn-slide">
+                                        {slide.buttonText}
+                                    </a>
+                                ) : (
+                                    <button className="btn-slide" onClick={showModal} style={{ border: 'none', cursor: "pointer", fontWeight: 'bold', fontSize:"1rem"}}>{slide.buttonText}</button>
+                                )}
                             </div>
                         </div>
-                    </div>
-
-                </SwiperSlide>
-                <SwiperSlide
-                    style={{
-                        backgroundImage: "url(/Puente_Bellote.png)",
-                        // backgroundPosition: "center",
-                        backgroundAttachment: "contain",
-                        backgroundSize: "cover",
-                        backgroundRepeat: "no-repeat",
-                        // background: "linear-gradient(147deg,rgb(230, 228, 227) 0%,rgb(7, 7, 7) 74%)",
-                    }}
-                >
-                    <div className="cajaSombra">
-                        <div className="container containerTextos">
-                            <div className="contenedorCaja">
-                                <h2 className="swiperTextoTitulo" >Puente el Bellote</h2>
-                                <h3 className="swiperTextoSubTitulo">PUENTE EL BELLOTE</h3>
-                                <p className="swiperParrafo">
-                                    Visita nuestros atractivos turísticos.
-                                </p>
-                                <button className="swiperBoton"
-                                    onClick={() => console.log("hola2")}
-                                >Conoce más</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-
-
-                <SwiperSlide
-                    style={{
-                        backgroundImage: "url(/Cangrejo_Ceiba.png)",
-                        backgroundPosition: "center",
-                        backgroundAttachment: "contain",
-                        backgroundSize: "cover",
-                        backgroundRepeat: "no-repeat",
-                        // background: "linear-gradient(147deg,rgb(230, 228, 227) 0%,rgb(7, 7, 7) 74%)",
-                    }}
-                >
-                    <div className="cajaSombra">
-                        <div className="container containerTextos">
-                            <div className="contenedorCaja">
-                                <h2 className="swiperTextoTitulo" >Puerto Ceiba</h2>
-                                <h3 className="swiperTextoSubTitulo">MALECÓN DEL CANGREJO AZUL</h3>
-                                <p className="swiperParrafo">
-                                    Visita nuestros atractivos turísticos y disfruta de la gastronomía local.
-                                </p>
-                                <button className="swiperBoton"
-                                    onClick={() => console.log("hola2")}
-                                >Conoce más</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-
-                <SwiperSlide
-                    style={{
-                        backgroundImage: "url(https://hebbkx1anhila5yf.public.blob.vercel-storage.com/481786481_122151543080505987_5429321483106752167_n.jpg-BGmVUC87kptJy3gLnRRbF8qolsTtjk.jpeg)",
-                        backgroundPosition: "center",
-                        backgroundAttachment: "contain",
-                        backgroundSize: "cover",
-                        backgroundRepeat: "no-repeat",
-                        // background: "linear-gradient(147deg,rgb(230, 228, 227) 0%,rgb(7, 7, 7) 74%)",
-                    }}
-                >
-                    <div className="cajaSombra">
-                        <div className="container containerTextos">
-                            <div className="contenedorCaja">
-                                <h2 className="swiperTextoTitulo" >Feria Paraíso 2025</h2>
-                                <h3 className="swiperTextoSubTitulo">DEL 20 AL 26 DE ABRIL</h3>
-                                <p className="swiperParrafo">
-                                    Disfruta de eventos culturales, artísticos y gastronómicos para toda la familia.
-                                </p>
-                                <button className="swiperBoton"
-                                    onClick={() => console.log("hola")}
-                                >Conoce más</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </SwiperSlide>
-            </Swiper >
-        </>
-    )
-}
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
